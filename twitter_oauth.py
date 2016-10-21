02d3a73cf0e910db72b4ef2f0e3590c22fd1d8a5
Akinwande Atanda <tripplehay777@gmail.com>
2016-10-22 12:41:17 +1300

    Add files via upload

---
+++ twitter_oauth.py
@@ -0,0 +1,3 @@
+# Databricks notebook source exported at Fri, 21 Oct 2016 23:40:08 UTC
+OAUTH_TOKEN = '1195073922-M9us9fwqLf2YL7yTECl4NVzrQc6qoOFFXYVQfdC'
+OAUTH_TOKEN_SECRET = 'm2PJ0HVzJgTwhNlxQ9Nga2qYNhRarQ0dKb36ggFZkkaUc'
